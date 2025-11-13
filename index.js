fbbee412bbb4304463b4a83e0ec3576966256f14
Md Hasibur Rahman <hasib2920@hotmail.com>
2025-11-13 12:59:17 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
+
+//pass: Uj7puBxkuh1C3Tal
+
+const uri =
+  "mongodb+srv://event-db:Uj7puBxkuh1C3Tal@cluster0.uxvhhti.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Ensures that the client will close when you finish/error
+// await client.close();
 
 app.get("/", (req, res) => {
   res.send("server is running fine");
