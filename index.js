f79e1b75a9a7192983770aba9c8b386c4e3b8ca0
Md Hasibur Rahman <hasib2920@hotmail.com>
2025-11-13 16:22:15 +0600

    get the data from MongoDB

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 const uri =
   "mongodb+srv://event-db:Uj7puBxkuh1C3Tal@cluster0.uxvhhti.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,9 +21,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("event-db");
+    const eventCollection = db.collection("events");
+
+    app.get("/events", async (req, res) => {
+      const result = await eventCollection.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
